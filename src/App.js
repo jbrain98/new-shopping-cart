6d5b4e20a33ff99a177d2d04bedbca9ab54c0fe6
Julia <juliabrain2020@u.northwestern.edu>
2020-01-01 11:48:00 -0600

    pics and names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title, Column } from 'rbx';
+import { Button, Container, Title, Column, Card, Image } from 'rbx';
 
 const sizes = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL']
 
@@ -15,7 +15,10 @@ const SizeContainer = () => {
 }
 
 const GithubReviewBox = () => (
-  <h2 style={{textAlign: 'center', color: 'gray'}}>Leave a start on Github if this repository was useful :)</h2>
+  <h2
+    style={{ textAlign: 'center', color: 'gray' }}>
+    Leave a star on Github if this repository was useful :)
+  </h2>
 )
 
 const SizeHeader = () => (
@@ -24,26 +27,65 @@ const SizeHeader = () => (
 
 const SizeButtons = () => (
   <React.Fragment>
-        { Object.values(sizes).map(value => <button style={{margin: '5px', border: '0px', width: '40px', height: '40px', borderRadius: '100%', backgroundColor: '#DDDDDD'}} key={value}> {value}</button>)}
+    {Object.values(sizes).map(value =>
+      <Button
+        style={{ margin: '5px', border: '0px', width: '40px', height: '40px', borderRadius: '100%', backgroundColor: '#DDDDDD' }}
+        key={value}>
+        {value}
+      </Button>)}
   </React.Fragment>
 )
 
-const ProductsContainer = ({products}) => (
-  <React.Fragment>
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
-  </React.Fragment>
+const ProductsContainer = ({ products }) => (
+  <Column.Group vcentered multiline>
+
+    {products.map(product =>
+      <Column
+        fullhd={{ size: 'one-quarter' }}
+        widescreen={{ size: 'one-quarter' }}
+        desktop={{ size: 'one-third' }}
+        tablet={{ size: 'half' }}
+        mobile={{ size: 'full' }}
+      >
+        <ProductCard product={product} />
+      </Column>
+    )}
+
+  </Column.Group>
+)
+
+const ProductCard = ({ product }) => (
+  <Card>
+    <Card.Content>
+      <ProductPicture product={product} />
+    </Card.Content>
+    <Card.Content>
+      <ProductTitle product={product} />
+    </Card.Content>
+  </Card>
+)
+
+const ProductTitle = ({ product }) => (
+  <Title size={5} style={{ textAlign: 'center'}}> {product.title} </Title>
+)
+
+
+const ProductPicture = ({ product }) => (
+  <Card.Image>
+    <Image.Container>
+      <Image src={`data/products/${product.sku}_1.jpg`} />
+    </Image.Container>
+  </Card.Image>
 )
 
 const ShoppingPage = ({ products }) => (
   <Column.Group>
-  <Column size = {1} />
-  <Column size= {2}>
-    <SizeContainer />
-  </Column>
-  <Column size="three-fifth">
-    <ProductsContainer products = {products}/>
+    <Column size={1} />
+    <Column size={2}>
+      <SizeContainer />
+    </Column>
+    <Column size={8}>
+      <ProductsContainer products={products} />
     </Column>
   </Column.Group>
 )
@@ -61,7 +103,7 @@ const App = () => {
   }, []);
 
   return (
-    <ShoppingPage products = {products}/>
+    <ShoppingPage products={products} />
   );
 };
 
