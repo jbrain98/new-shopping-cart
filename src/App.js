73b40acff0bd2e4022c1e025c7a14279d7666796
Julia <juliabrain2020@u.northwestern.edu>
2020-01-21 20:22:57 -0600

    static shopping cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title, Column, Card, Image } from 'rbx';
+import { Button, Container, Title, Column, Card, Image, Media, Content } from 'rbx';
+import Sidebar from "react-sidebar";
 
-const sizes = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL']
+const sizes = ['S', 'M', 'L', 'XL']
 
 const SizeContainer = () => {
   return (
@@ -61,12 +62,23 @@ const ProductCard = ({ product }) => (
     </Card.Content>
     <Card.Content>
       <ProductTitle product={product} />
+      <ProductDescription product={product} />
+      <ProductPrice product={product} />
+      <SizeButtons />
     </Card.Content>
   </Card>
 )
 
 const ProductTitle = ({ product }) => (
   <Title size={5} style={{ textAlign: 'center'}}> {product.title} </Title>
+)
+
+const ProductDescription = ({ product }) => (
+  <Title size={6} style={{ textAlign: 'center'}}> {product.description} </Title>
+)
+
+const ProductPrice = ({ product }) => (
+  <Title size={6} style={{ textAlign: 'center'}}> {`${product.currencyFormat}${product.price}`} </Title>
 )
 
 
@@ -80,17 +92,87 @@ const ProductPicture = ({ product }) => (
 
 const ShoppingPage = ({ products }) => (
   <Column.Group>
-    <Column size={1} />
-    <Column size={2}>
-      <SizeContainer />
-    </Column>
+    <Column size={2} />
     <Column size={8}>
       <ProductsContainer products={products} />
     </Column>
   </Column.Group>
 )
 
+const ShoppingCart = ({products}) => {
+  const [sidebarState, sidebarSwitch] = useState({sidebarOpen: false});
+  {products.map(product =>
+      <ProductCard product={product} />
+  )}
+  const [cartItems, changeCart] = useState([]);//[products[0], products[1]]);
+  const onSetSidebarOpen = ({open}) => {
+    var nextstate = true;
+    if (sidebarState.sidebarOpen == true)
+    {
+      nextstate = false
+    }
+    sidebarSwitch({ sidebarOpen: nextstate });
+  };
+    return(
+      <Sidebar sidebar = {<ShoppingCartList selected = {products}></ShoppingCartList>}
+      open = {sidebarState.sidebarOpen}
+      onSetOpen = {onSetSidebarOpen} 
+      styles={{ sidebar: { background: "white" } }}
+      > <button onClick={() => onSetSidebarOpen(true)}>
+      <Title>Cart</Title>
+    </button></Sidebar>
+    );
+
+}
+
+
+const ShoppingCartList = ({selected}) => 
+{
+  var totalPrice = 0;
+  return (
+  <Container>
+
+    {selected.map(product =>
+        {totalPrice += product.price;
+        return(
+          <ShoppingCartItem product={product} />
+        )}
+      
+    )}
+  <Title>Total Price: ${totalPrice}</Title>
+  </Container>
+)
+    }
+
+
+const ShoppingCartItem = ({product}) =>(
+  <Media>
+  <Media.Item as="figure" align="left">
+    <Image.Container>
+      <Image
+        src={`data/products/${product.sku}_2.jpg`}/>
+    </Image.Container>
+  </Media.Item>
+  <Media.Item align="content">
+    <Content>
+      <h6>
+        <h5>{product.title}</h5>
+        <br />
+        {product.description}
+      </h6>
+    </Content>
+  </Media.Item>
+  <Media.Item align="right">
+    <Button> X </Button>
+    <br />
+    <h3> {product.price} </h3>
+  </Media.Item>
+</Media>
+)
+
+
 const App = () => {
+  
   const [data, setData] = useState({});
   const products = Object.values(data);
   useEffect(() => {
@@ -103,7 +185,11 @@ const App = () => {
   }, []);
 
   return (
-    <ShoppingPage products={products} />
+    <React.Fragment>
+      <ShoppingPage products={products} />
+      <ShoppingCart products= {products}/>
+    </React.Fragment>
+    
   );
 };
 
